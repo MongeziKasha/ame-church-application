46bd8af24e87a1955cd4769e665c8c2683c7dc15
Mongezi Kasha <Mongezi.Kasha@investec.com>
2025-03-12 15:17:47 +0200

    Made http calls post and get

--- src/CustomerApp/Customer/CustomerApp.CustomerComponent.ts
+++ src/CustomerApp/Customer/CustomerApp.CustomerComponent.ts
@@ -3,8 +3,11 @@ import { FormsModule } from '@angular/forms';
 import { CommonModule } from '@angular/common';
 import { ReactiveFormsModule } from '@angular/forms';
 import { BaseLogger } from '../Utility/CustomerApp.Logger';
-import { Customer } from '../Customer/CustomerApp.model'; // Adjust the import path as necessary
+import { Customer } from '../Customer/CustomerApp.model';
 import { GridComponent } from '../Utility/CustomerApp.Grid.Component';
+import { HttpClient } from '@angular/common/http'; 
+import { catchError, tap } from 'rxjs/operators'; 
+import { of } from 'rxjs'; 
 
 @Component({
     templateUrl: './CustomerApp.CustomerView.html',
@@ -16,16 +19,60 @@ export class CustomerComponent {
     CustomerModel: Customer = new Customer();
     CustomerModels: Array<Customer> = [];
     _log!: BaseLogger;
-
-    constructor(_logger: BaseLogger) {
+    Disable:boolean = false;
+    
+    constructor(_logger: BaseLogger, public http: HttpClient) {
         this._log = _logger;
         this._log.Log();
     }
 
-    SelectCustomer(_selected: Customer) { // Ensure this parameter is of type Customer
+    SelectCustomer(_selected: Customer) {
         this.CustomerModel = _selected;
     }
 
+    postToServer() {
+        const custDto = {
+            CustomerCode: this.CustomerModel.CustomerCode,
+            CustomerName: this.CustomerModel.CustomerName,
+            CustomerAmount: this.CustomerModel.CustomerAmount
+        };
+        this.Disable= true;
+        this.http.post("http://localhost:3000/customers", custDto)
+            .pipe(
+                tap((res: any) => this.Success(res)), 
+                catchError((error) => {
+                    this.Error(error); 
+                    return of(null); 
+                })
+            )
+            .subscribe();
+    }
+    getFromServer() {
+
+        this.http.get("http://localhost:3000/customers")
+            .pipe(
+                tap((res: any) => this.Success(res)), 
+                catchError((error) => {
+                    this.Error(error); 
+                    return of(null); 
+                })
+            )
+            .subscribe();
+    }
+    Error(res: any) {
+        console.error('Error:', res); 
+    }
+
+    Success(res: any) {
+       this.getFromServer();
+       this.Disable=false;
+       this.CustomerModel = new Customer();
+    }
+    SuccessGet(res:any){
+        if (res) {
+            this.CustomerModels.push(res); 
+        }
+    }
     Add() {
         this.CustomerModels.push({ ...this.CustomerModel });
         this.CustomerModel = new Customer();
--- src/main.ts
+++ src/main.ts
@@ -5,11 +5,12 @@ import { RouterModule } from '@angular/router';
 import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 import { MainRoutes } from './CustomerApp/Routing/CustomerApp.MainRouting'; // Ensure this path is correct
 import { BaseLogger, DbLogger } from './CustomerApp/Utility/CustomerApp.Logger';
+import { HttpClient, HttpClientModule } from '@angular/common/http'; 
 
 // Bootstrap the standalone component (MasterComponent)
 bootstrapApplication(MasterComponent, {
   providers: [
-    importProvidersFrom(FormsModule, ReactiveFormsModule, RouterModule.forRoot(MainRoutes)),
+    importProvidersFrom(FormsModule, ReactiveFormsModule, HttpClient, HttpClientModule, RouterModule.forRoot(MainRoutes)),
     {provide:BaseLogger, useClass:DbLogger} // Remove BrowserModule
   ]
 }).catch(err => console.error(err));
